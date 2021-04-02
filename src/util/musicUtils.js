7fa9c535dfeced5bf2aa51f1ddf791a94e59919e
cheolho <beakya884@gmail.com>
2021-04-02 21:21:32 +0900

    음악 파일에 벅스 id, youtube id tag로 추가하게 변경.

--- src/util/musicUtils.js
+++ src/util/musicUtils.js
@@ -1,9 +1,11 @@
 import ID3Writer from 'browser-id3-writer';
 import fs from 'fs';
-export const writeMetaData = (musicFilePath, title, artist, album, imageData, lyrics = '') => {
+export const writeMetaData = (musicFilePath, title, artist, album, imageData, lyrics = '', bugsId, youtubeId) => {
   const songBuffer = fs.readFileSync(musicFilePath);
   const writer = new ID3Writer(songBuffer);
   writer.setFrame('TIT2', title)
+    .setFrame('WORS', bugsId)
+    .setFrame('WOAS', youtubeId)
     .setFrame('TPE1', [artist])
     .setFrame('TALB', album)
     .setFrame('APIC', {
